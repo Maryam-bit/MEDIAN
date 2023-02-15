d76793e676a38ff31ca18d928c691a76abdfbeb0
Maryam-bit <rajputmaryam5555@gmail.com>
2023-02-15 20:59:51 +0500

    feat: added prisma client to the articles  module

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [ArticlesController],
-  providers: [ArticlesService]
+  providers: [ArticlesService],
+  imports: [PrismaModule],
 })
 export class ArticlesModule {}
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ArticlesService {
+  constructor(private prisma: PrismaService) {}
   create(createArticleDto: CreateArticleDto) {
     return 'This action adds a new article';
   }
