d3ec1d8965dbdd61d2cbde5f46699a053d4ad55a
Maryam-bit <rajputmaryam5555@gmail.com>
2023-02-15 21:02:11 +0500

    chore: linting

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
