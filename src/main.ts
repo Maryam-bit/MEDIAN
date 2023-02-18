370ea3727e7e94308619be4d86b7ca2d30e65bdf
Maryam-bit <rajputmaryam5555@gmail.com>
2023-02-18 23:52:22 +0500

    auth module initialized

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strip out the elements that are not defined in our dto
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('Median')
     .setDescription('The Median API description')
