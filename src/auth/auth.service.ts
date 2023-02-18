6f63fbd9ddec98e5973eacb1aace8baaa7f335dc
Maryam-bit <rajputmaryam5555@gmail.com>
2023-02-19 03:31:59 +0500

    signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from './../prisma/prisma.service';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
-  signup(authDto: AuthDto) {
-    return 'This action adds a new auth';
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    // generate the password
+    const hash = await argon.hash(dto.password);
+    try {
+      // save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+
+      // remove password from response
+      delete user.password;
+      // return the saved user
+      return user;
+    } catch (error) {
+      console.log(error);
+      if (error instanceof PrismaClientKnownRequestError) {
+        // catching prisma related error <> duplicate field in our case
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Crendentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  signin(authDto: AuthDto) {
+  signin(dto: AuthDto) {
     return `This action returns all auth`;
   }
 }
